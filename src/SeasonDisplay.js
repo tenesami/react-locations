64302065da4cf5957b594f21c25e3ab962368462
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-01-17 20:52:56 -0700

    state and class lable lifecycle

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SeasonDisplay = () => {
+    return (
+        <div>
+            Season Desplay 
+        </div>
+    )
+}
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
+
+class App extends React.Component {
+  constructor(props){
+    //uses to refernce the parent of constructor class 
+    super(props)
+
+    //since we don't know the value of lat we intialize to null
+    this.state = {lat: null, errorMessage: ''};
+
+  }
+
+  componentDidMount(){
+
+    window.navigator.geolocation.getCurrentPosition (
+      postition => this.setState({lat: postition.coords.latitude}),
+      err => this.setState({errorMessage: err.message})
+    );
+  }
+  //render is for react requirment 
+  render(){
+    if(this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if(!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }  
+    return<div>Loading! </div>;
+  };
+}
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<App />,
+
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
