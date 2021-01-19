079092db2234236f1deba29ba8ea4ba5100a4bc5
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-01-18 22:21:04 -0700

    adding the class lifesycle using browser locaiton and styleing the page

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,36 @@
+import './SeasonDisplay.css';
 import React from 'react'
 
-const SeasonDisplay = () => {
+const seasonConfig = {
+    summer: {
+        text: 'let\'s workout out side',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'let\'s workout inside',
+        iconName: 'snowflake'
+
+    }
+};
+
+const getSeason = (lat, month) => {
+    if (month > 0  && month < 9){
+        return lat > 0 ? 'summer' : 'winter';
+    }
+    else {
+        return lat > 0 ? 'winter' : 'summer'
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season];
+    
     return (
-        <div>
-            Season Desplay 
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`} />
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  constructor(props){
-    //uses to refernce the parent of constructor class 
-    super(props)
+  //bable will built constructor for us 
+    state = {lat: null, errorMessage: ''};
 
-    //since we don't know the value of lat we intialize to null
-    this.state = {lat: null, errorMessage: ''};
-
-  }
-
-  componentDidMount(){
-
+  componentDidMount (){
     window.navigator.geolocation.getCurrentPosition (
       postition => this.setState({lat: postition.coords.latitude}),
       err => this.setState({errorMessage: err.message})
     );
   }
-  //render is for react requirment 
-  render(){
-    if(this.state.errorMessage && !this.state.lat){
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
 
-    if(!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
-    }  
-    return<div>Loading! </div>;
-  };
+  renderContent (){
+      if(this.state.errorMessage && !this.state.lat){
+        return <div>Error: {this.state.errorMessage}</div>;
+      }
+
+      if(!this.state.errorMessage && this.state.lat) {
+        return <SeasonDisplay lat={this.state.lat}/>
+      }  
+      return <Spinner message="please accept location request to see the page" />;
+   }
+  
+    render(){
+      return (
+        <div className="border red">
+          {this.renderContent()}
+        </div>
+      )
+  }
 }
-
-
 
 
 ReactDOM.render(
